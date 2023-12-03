b4eeaced753f37da704b1014af462d3e78e421a5
BADUGII <magic7925@gmail.com>
2023-12-03 17:46:27 +0900

    최종

--- HNU_OVERCLOCK_PROJECT_2/jjuggumi.h
+++ HNU_OVERCLOCK_PROJECT_2/jjuggumi.h
@@ -6,31 +6,13 @@
 
 #define PLAYER_MAX		10
 
-
-typedef struct {
-	char name[100];
-	int intel_buf, str_buf, stamina_buf;
-}ITEM;
-
-typedef struct {
-	int id;
-	char name[100];
-
-	int intel, str, stamina;
-
-	bool is_alive;
-	bool hasitem;
-	ITEM item;
-} PLAYER;
-
-PLAYER player[PLAYER_MAX]; // 기본값 true, 탈락하면 false
+bool player[PLAYER_MAX];  // 기본값 true, 탈락하면 false
 int n_player, n_alive, winner_player;
 int tick;  // 시계
 int stop_tick;
 bool yh_stop;
 
 // 미니게임
-void sample();
 void mugunghwa(void);
 //void nightgame(void);
 //void juldarigi(void);

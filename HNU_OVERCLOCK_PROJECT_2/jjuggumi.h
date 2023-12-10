ddf0f344475f9493ef5c81fb763eae021bf224f3
BADUGII <magic7925@gmail.com>
2023-12-10 23:52:40 +0900

    줄다리기

--- HNU_OVERCLOCK_PROJECT_2/jjuggumi.h
+++ HNU_OVERCLOCK_PROJECT_2/jjuggumi.h
@@ -31,6 +31,7 @@ PLAYER player[PLAYER_MAX]; // 기본값 true, 탈락하면 false => player[i].is_alive
 int n_player, n_alive, n_alive_arr[PLAYER_MAX], winner_player;
 int n_item;
 int tick;  // 시계
+int skill_tick;
 int stop_tick;
 bool yh_stop;
 
--- HNU_OVERCLOCK_PROJECT_2/juldarigi.c
+++ HNU_OVERCLOCK_PROJECT_2/juldarigi.c
@@ -13,16 +13,18 @@
 
 int px[PLAYER_MAX], py[PLAYER_MAX];
 int lx[3], ly[3];
-double str = 0, str_r = 0, str_l = 0;
+double str = 0, str_r = 0, str_l = 0, tmp_str;
+bool stop_or_go; //더 땡길지 말지 정하는 신호
+int r_skill = 1, l_skill = 1;
 
-void juldarigi_init(void);
-void print_str(void);
-void juldarigi_key_manual(void);
-void juldarigi_right(void);
-void juldarigi_left(void);
-void juldarigi_line(void);
-void juldarigi_move_tail(int i, int nx, int ny);
-void move_tail_for_line(int line, int nx, int ny);
+void juldarigi_init(void); //줄다리기 인트로
+void print_str(void); //str 출력
+void juldarigi_move_tail(int i, int nx, int ny); //줄다리기용 move_tail
+void move_tail_for_line(int line, int nx, int ny); //줄다리기 '-' 전용 move_tail
+void juldarigi_right(void); //오른쪽으로 당기는 함수
+void juldarigi_left(void); //왼쪽으로 당기는 함수
+void left_skill(void);
+void right_skill(void);
 
 void juldarigi_init(void) {
 	map_init_sharp(3, 29);
@@ -59,10 +61,14 @@ void juldarigi_init(void) {
 
 		back_buf[px[i]][py[i]] = '0' + i;  // (0 .. n_player-1)
 	}
+	stop_or_go = true;
 	tick = 0;
 }
 
 void print_str(void) {
+	str = 0;
+	str_r = 0;
+	str_l = 0;
 	for (int i = 0; i < n_player; i++) {
 		if (i % 2 == 0) {
 			str_r += player[i].str;
@@ -71,17 +77,26 @@ void print_str(void) {
 			str_l += player[i].str;
 		}
 	}
-	str = str_r - str_l;
-	printf("\n\n\n\nstr:%5.1f\n\n", str);
+	str = (-str_r) + str_l;
+	gotoxy(N_ROW + 1, 0);
+	printf("str:%5.1f", str);
 }
 
 bool juldarigi_placable(int y) {
-	if (y == 0 || y == 28) {
+	if (y == 6 || y == 22) {
 		return false;
 	}
 	else {
 		return true;
 	}
+}
+
+void juldarigi_move_tail(int player, int nx, int ny) {
+	int p = player;
+	back_buf[nx][ny] = back_buf[px[p]][py[p]];
+	back_buf[px[p]][py[p]] = ' ';
+	px[p] = nx;
+	py[p] = ny;
 }
 
 void move_tail_for_line(int line, int nx, int ny) {
@@ -92,33 +107,33 @@ void move_tail_for_line(int line, int nx, int ny) {
 	ly[l] = ny;
 }
 
-void juldarigi_key_manual(void) {
-
-}
-
 void juldarigi_right(void) {
 	int nx, ny;
-	for (int i = n_player - 1; i > 0; i -= 2) {
-		nx = px[i] + 0;
-		ny = py[i] + 1;
-		if (juldarigi_placable(ny) == false) {
-			break;
+	if (stop_or_go == true) {
+		for (int i = n_player - 1; i > 0; i -= 2) {
+			nx = px[i] + 0;
+			ny = py[i] + 1;
+			if (juldarigi_placable(ny) == false) {
+				break;
+			}
+			juldarigi_move_tail(i, nx, ny);
 		}
-		juldarigi_move_tail(i, nx, ny);
-	}
-	for (int i = 2; i >= 0; i--) {
-		nx = lx[i] + 0;
-		ny = ly[i] + 1;
-		if (!placable(nx, ny)) {
-			break;
+		for (int i = 2; i >= 0; i--) {
+			nx = lx[i] + 0;
+			ny = ly[i] + 1;
+			if (!placable(nx, ny)) {
+				break;
+			}
+			move_tail_for_line(i, nx, ny);
 		}
-		move_tail_for_line (i, nx, ny);
 	}
 	for (int i = 0; i <= n_player - 2; i += 2) {
 		nx = px[i] + 0;
 		ny = py[i] + 1;
-		if (!placable(nx, ny)) {
-			break;
+		if (ny == 14) {
+			stop_or_go = false;
+			gotoxy(N_ROW + 1, 0);
+			continue;
 		}
 		juldarigi_move_tail(i, nx, ny);
 	}
@@ -126,55 +141,105 @@ void juldarigi_right(void) {
 
 void juldarigi_left(void) {
 	int nx, ny;
-	for (int i = n_player - 2; i >= 0; i -= 2) {
+	if (stop_or_go == true) {
+		for (int i = n_player - 2; i >= 0; i -= 2) {
+			nx = px[i] + 0;
+			ny = py[i] - 1;
+			if (juldarigi_placable(ny) == false) {
+				break;
+			}
+			juldarigi_move_tail(i, nx, ny);
+		}
+		for (int i = 0; i < 3; i++) {
+			nx = lx[i] + 0;
+			ny = ly[i] - 1;
+			if (!placable(nx, ny)) {
+				break;
+			}
+			move_tail_for_line(i, nx, ny);
+		}
+	}
+	for (int i = 1; i <= n_player-1; i += 2) {
 		nx = px[i] + 0;
 		ny = py[i] - 1;
-		if (juldarigi_placable(ny) == false) {
-			break;
-		}
-		juldarigi_move_tail(i, nx, ny);
-	}
-	for (int i = 0; i < 3; i++) {
-		nx = lx[i] + 0;
-		ny = ly[i] - 1;
-		if (!placable(nx, ny)) {
-			break;
-		}
-		move_tail_for_line(i, nx, ny);
-	}
-	for (int i = 1; i <= n_player - 1; i += 2) {
-		nx = px[i] + 0;
-		ny = py[i] - 1;
-		if (!placable(nx, ny)) {
-			break;
+		if (ny == 14) {
+			stop_or_go = false;
+			gotoxy(N_ROW + 1, 0);
+			continue;
 		}
 		juldarigi_move_tail(i, nx, ny);
 	}
 }
 
-void juldarigi_move_tail(int player, int nx, int ny) {
-	int p = player;  // 이름이 길어서...
-	back_buf[nx][ny] = back_buf[px[p]][py[p]];
-	back_buf[px[p]][py[p]] = ' ';
-	px[p] = nx;
-	py[p] = ny;
+void left_skill(void) {
+	for (int i = 0; i <= n_player - 2; i += 2) {
+		player[i].str *= 2;
+		gotoxy(N_ROW, 0);
+		printf("짝수팀 눕기 사용!");
+	}
+}
+
+void right_skill(void) {
+	for (int i = 1; i <= n_player - 1; i += 2) {
+		player[i].str *= 2;
+		gotoxy(N_ROW, 0);
+		printf("홀수팀 눕기 사용!");
+	}
 }
 
 void juldarigi(void) {
 	juldarigi_init();
-	//dialog(" -준비- ");
 	print_str();
+	tmp_str = str;
 	display();
+	dialog(" -준비- ");
 	while (1) {
+		key_t key = get_key();
+		if (key == K_QUIT) {
+			break;
+		}
+		else if (key == K_STR_DOWN) {
+			tmp_str -= 1;
+		}
+		else if (key == K_STR_UP) {
+			tmp_str += 1;
+		}
+		else if (key == K_L_SKILL) {
+			left_skill();
+			l_skill = 2;
+		}
+		else if (key == K_R_SKILL) {
+			right_skill();
+			r_skill = 2;
+		}
 		print_str();
-
-		if (tick / 3000 == 1) {
-			if (str > 0) {
-				juldarigi_right();
+		if (tick % 3000 == 0) {
+			if (tmp_str > 0) {
+				if (r_skill == 2) {
+					juldarigi_right();
+					juldarigi_right();
+					r_skill = 1;
+					for (int i = 1; i <= n_player - 1; i += 2) {
+						player[i].str /= 2;
+					}
+				}
+				else {
+					juldarigi_right();
+				}
 				tick = 0;
 			}
-			else if (str < 0) {
-				juldarigi_left();
+			else if (tmp_str < 0) {
+				if (l_skill == 2) {
+					juldarigi_left();
+					juldarigi_left();
+					l_skill = 1;
+					for (int i = 0; i <= n_player - 2; i += 2) {
+						player[i].str /= 2;
+					}
+				}
+				else {
+					juldarigi_left();
+				}
 				tick = 0;
 			}
 		}
